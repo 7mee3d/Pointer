65450b2f36f3ce45f0074ede389436a9bf44905e
7mee3d <alanturing404@gmail.com>
2025-03-23 16:34:13 +0200

    cheak another pointer is null pointer or not

--- 8- Type of Pointer/Pointer Null/Pointer Null 2/Pointer Null 2.cpp
+++ 8- Type of Pointer/Pointer Null/Pointer Null 2/Pointer Null 2.cpp
@@ -18,13 +18,13 @@ int main()
 
    // to solve warring or error using ( default pointer null ) 
 
-    int* pointer = nullptr;
+    int* pointer1 = nullptr;
     int* pointer2 = NULL;
     int* pointer3 = 0; 
 
     // this is default pointer is empty not including value 
 
-    cout << pointer << endl; 
+    cout << pointer1 << endl; 
     cout << pointer2 << endl; 
     cout << pointer3 << endl; 
 
@@ -32,8 +32,17 @@ int main()
 
     // cheak pointer is empty 
 
-    if (pointer == nullptr)
-        cout << "The Pointer is empty \n";
 
+    if (pointer1 == nullptr)
+        cout << "The Pointer 1 is empty \n";
+
+    if (pointer2 == nullptr) 
+        cout << "The Pointer 2 is empty \n";
+
+    if (pointer3 == nullptr)
+        cout << "The Pointer 3 is empty \n";
+
+
+    cout << "Thanks \n"; 
 }
 
